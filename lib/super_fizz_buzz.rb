5be2e0d204a498fe93b725ce97342fd9f8a87a58
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-07 09:28:53 -0500

    keep a revert spot if class breaks

--- lib/super_fizz_buzz.rb
+++ lib/super_fizz_buzz.rb
@@ -1,2 +1,24 @@
-# Iterate through the numbers 1 to 1000
-# For each one, print the output according to the rules of Super Fizz Buzz
+for number in 1..1000 do
+  if number % 3 == 0
+    if number % 5 == 0
+      if number % 7 == 0
+        puts 'SuperFizzBuzz'
+      else
+        puts 'FizzBuzz'
+      end
+    elsif number % 7 == 0
+      puts 'SuperFizz'
+    else puts 'Fizz'
+    end
+  elsif number % 5 == 0
+    if number % 7 == 0
+      puts 'SuperBuzz'
+    else
+      puts 'Buzz'
+    end
+  elsif number % 7 == 0
+    puts 'Super'
+  else
+    puts number
+  end
+end
